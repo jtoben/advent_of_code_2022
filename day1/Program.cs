8fe8686841dac70b4e6ba1b332963dd29147b227
Joost Toben <joost.toben@rabobank.nl>
2022-12-01 08:57:10 +0100

    add solution day1

---
+++ day1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var caloriesPerElf = File.ReadAllText("input.txt")
+                .Split("\n\n")
+                .Select(x => x.Split("\n").Select(int.Parse));
+            
+            Console.WriteLine(PartOne(caloriesPerElf));
+            Console.WriteLine(PartTwo(caloriesPerElf));
+        }
+
+        private static int PartOne(IEnumerable<IEnumerable<int>> caloriesPerElf) {
+            return caloriesPerElf.Select(x => x.Sum()).Max();
+        }
+
+        private static int PartTwo(IEnumerable<IEnumerable<int>> caloriesPerElf) {
+            return caloriesPerElf.Select(x => x.Sum()).OrderByDescending(x => x).Take(3).Sum();
+        }
+    }
+}
