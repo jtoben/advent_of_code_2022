9353c36395dad71d8334649d49f9d64e9d91e037
Joost Toben <joost.toben@rabobank.nl>
2022-12-10 15:02:12 +0100

    Add solution day8 and day9.

---
+++ day8/Program.cs
@@ -0,0 +1,119 @@
+﻿namespace Day8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("input.txt");
+
+            var forestMap = new int[input[0].Length, input.Length];
+            for (int x = 0; x < input[0].Length; x++) {
+                for (int y = 0; y < input.Length; y++) {
+                    forestMap[x, y] = int.Parse(input[y][x].ToString());
+                }
+            }
+
+            Console.WriteLine(PartOne(forestMap));
+            Console.WriteLine(PartTwo(forestMap));
+        }
+
+        private static int PartOne(int[,] forestMap) {
+            int visibleTreeCount = 0;
+            for (int x = 1; x < forestMap.GetLength(0) - 1; x++) {
+                for (int y = 1; y < forestMap.GetLength(1) - 1; y++) {
+                    if (isTreeVisible(forestMap, (x, y))) {
+                        visibleTreeCount++;
+                    }
+                }
+            }
+
+            return visibleTreeCount + (forestMap.GetLength(0) * 2) + (forestMap.GetLength(1) * 2) - 4;
+        }
+
+        private static int PartTwo(int[,] forestMap) {
+            List<(int X, int Y)> visibleTreeLocations = new();
+            for (int x = 1; x < forestMap.GetLength(0) - 1; x++) {
+                for (int y = 1; y < forestMap.GetLength(1) - 1; y++) {
+                    if (isTreeVisible(forestMap, (x, y))) {
+                        visibleTreeLocations.Add((x, y));
+                    }
+                }
+            }
+
+            List<int> scenicScores = new();
+
+            foreach (var treeLocation in visibleTreeLocations) {
+                scenicScores.Add(GetScenicScoreForLocation(forestMap, treeLocation));
+            }
+
+            return scenicScores.Max();
+        }
+
+        private static bool isTreeVisible(int[,] forestMap, (int X, int Y) location) {
+            foreach (var direction in Enum.GetValues<Direction>()) {
+                if (CheckVisibilityForDirection(forestMap, location, direction)) {
+                    return true;
+                };
+            }
+            return false;
+        }
+
+        private static bool CheckVisibilityForDirection(int[,] forestMap, (int X, int Y) location, Direction direction) {
+            var treeHeight = forestMap[location.X, location.Y];
+            var trees = GetTreesForDirection(forestMap, location, direction);
+
+            return trees.Count(x => x < treeHeight) == trees.Count;
+        }
+
+        private static List<int> GetTreesForDirection(int[,] forestMap, (int X, int Y) location, Direction direction) {
+            List<int> trees = new();
+            
+            var offset = GetOffsetForDirection(direction);
+            do {
+                location = (location.X + offset.X, location.Y + offset.Y);
+                trees.Add(forestMap[location.X, location.Y]);
+            } while (location.X > 0 && location.X < forestMap.GetLength(0) - 1 && location.Y > 0 && location.Y < forestMap.GetLength(1) - 1);
+
+            return trees;
+        }
+
+        private static (int X, int Y) GetOffsetForDirection(Direction direction) {
+            return direction switch {
+                Direction.Up => (0, -1),
+                Direction.Right => (1, 0),
+                Direction.Down => (0, 1),
+                Direction.Left => (-1, 0),
+                _ => throw new Exception("undefined")
+            };
+        }
+
+        private static int GetScenicScoreForLocation(int[,] forestMap, (int X, int Y) location) {
+             var treeHeight = forestMap[location.X, location.Y];
+             
+             List<int> treeCounts = new();
+             foreach (var direction in Enum.GetValues<Direction>()) {
+                var trees = GetTreesForDirection(forestMap, location, direction);
+
+                int count = 0;
+                for (int i = 0; i < trees.Count; i++) {
+                    count++;
+
+                    if (treeHeight <= trees[i]) {
+                        break;
+                    }
+                }
+
+                treeCounts.Add(count);
+            }
+
+            return treeCounts.Aggregate((a, b) => a * b);
+        }
+
+        private enum Direction {
+            Up,
+            Right,
+            Down,
+            Left
+        }
+    }
+}
---
+++ day9/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace Day9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var instructions = File.ReadAllLines("input.txt")
+                .Select(x => (Direction: x.Split(" ")[0], Amount: int.Parse(x.Split(" ")[1])));
+            
+            Console.WriteLine(PartOne(instructions));
+            Console.WriteLine(PartTwo(instructions));
+        }
+
+        private static int PartOne(IEnumerable<(string Direction, int Amount)> instructions) {
+            return CalculateVisitedPositionCountForTail(2, instructions);
+        }
+
+        private static int PartTwo(IEnumerable<(string Direction, int Amount)> instructions) {
+            return CalculateVisitedPositionCountForTail(10, instructions);
+        }
+
+        private static int CalculateVisitedPositionCountForTail(int ropeLength, IEnumerable<(string Direction, int Amount)> instructions) {
+            var ropeParts = new List<Vector2>();
+            for (int i = 0; i < ropeLength; i++) {
+                ropeParts.Add(new Vector2 { X = 0, Y = 0 });
+            }
+
+            HashSet<Vector2> visitedTailPositions = new();
+            foreach (var instruction in instructions) {
+                var offset = GetOffsetForDirection(instruction.Direction);
+
+                for (int i = 0; i < instruction.Amount; i++) {
+                    ropeParts[0] += offset;
+                    for (int j = 1; j < ropeParts.Count; j++) {
+                        var tailOffset = GetOffsetForTail(ropeParts[j - 1], ropeParts[j]);
+                        ropeParts[j] += tailOffset;
+
+                        if (j == ropeParts.Count - 1) {
+                            visitedTailPositions.Add(ropeParts[j]);
+                        }
+                    }
+                }
+            }
+            
+            return visitedTailPositions.Count();
+        }
+
+        private static Vector2 GetOffsetForDirection(string direction) {
+            return direction switch {
+                "U" => new Vector2 { X = 0, Y = -1},
+                "R" => new Vector2 { X = 1, Y = 0},
+                "D" => new Vector2 { X = 0, Y = 1},
+                "L" => new Vector2 { X = -1, Y = 0},
+                _ => throw new Exception("undefined")
+            };
+        }
+
+        private static Vector2 GetOffsetForTail(Vector2 headPosition, Vector2 tailPosition) {
+            var xDistance = Math.Abs(headPosition.X - tailPosition.X);
+            var yDistance = Math.Abs(headPosition.Y - tailPosition.Y);
+
+            if (xDistance <= 1 && yDistance <= 1) {
+                return new Vector2 { X = 0, Y = 0 };
+            }
+
+            xDistance = Math.Clamp(headPosition.X - tailPosition.X, -1, 1);
+            yDistance = Math.Clamp(headPosition.Y - tailPosition.Y, -1, 1);
+
+            return new Vector2 { X = xDistance, Y = yDistance };
+        }
+
+
+        private record Vector2 {
+            public required int X {get; init; }
+            public required int Y {get; init; }
+
+            public static Vector2 operator +(Vector2 a, Vector2 b) => new Vector2 {X = a.X + b.X, Y = a.Y + b.Y};
+        }
+    }
+}
